e1ff26a27ebd389e08bd4d8c3b11a00b4a82f898
satishht <satish246.ht@gmail.com>
2024-06-21 23:40:22 +0530

    Largest Number in array

---
+++ LargestNumber.java
@@ -0,0 +1,33 @@
+package com.array.problems;
+
+/**
+ * Question :
+ * 1. Find the largest element in array
+ */
+
+/**
+ * @author Satish
+ */
+
+public class LargestNumber {
+
+	
+	public static void main(String[] args) {
+		
+	int a[]= {10,20,30};
+		
+		int total = a.length;
+		int temp;
+
+		for (int i = 0; i < total; i++) {
+			for (int j = i; j < total; j++) {
+				if (a[i] > a[j]) {
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		System.out.println( "Largest Number in array " + a[total - 1]);
+	}
+}
