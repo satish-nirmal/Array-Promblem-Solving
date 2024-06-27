9b7537f5b5ab49c86cf2e6c69f1d79374dac9771
satish-nirmal <satishnirmal16@gmail.com>
2024-06-27 21:07:50 +0530

    second smalleset element in array

---
+++ SecondSmallestElement.java
@@ -0,0 +1,23 @@
+package com.array.problems;
+
+public class SecondSmallestElement {
+	
+		    public static void main(String[] args) {
+		        
+		        int a[] = {10, 20, 30};
+		        
+		        int total = a.length;
+		        int temp;
+
+		        for (int i = 0; i < total; i++) {
+		            for (int j = i; j < total; j++) {
+		                if (a[i] < a[j]) {
+		                    temp = a[i];
+		                    a[i] = a[j];
+		                    a[j] = temp;
+		                }
+		            }
+		        }
+		        System.out.println("Second Smallest Number in array " + a[total - 2]);
+		    }
+		}
