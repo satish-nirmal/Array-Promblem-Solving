54b9fa9817b8ddbe49c67f34c4fa2791bf506543
satish-nirmal <satishnirmal16@gmail.com>
2024-07-01 22:02:37 +0530

    descending order for given array

---
+++ SortDecending.java
@@ -0,0 +1,39 @@
+package com.array.problems;
+
+/**
+ * Question :
+ * 1. Find the Descending order of array 
+ */
+
+/**
+ * @author Satish
+ */
+
+
+public class SortDecending {
+	
+	public static void main(String[] args) {
+		
+		
+	      int [] arr = new int [] {5, 2, 8, 7, 1};  
+	      int total = arr.length;
+	        int temp = 0;    
+	        
+	        for (int i = 0; i < total; i++) {     
+	            for (int j = i+1; j < total; j++) {     
+	               if(arr[i] < arr[j]) {    
+	                   temp = arr[i];    
+	                   arr[i] = arr[j];    
+	                   arr[j] = temp;    
+	               }     
+	            }     
+	        }    
+	 
+	        
+	        for(int i = 0 ; i < total ; i++) {
+	        	System.out.print(arr[i] + " ");
+	        }
+	}
+
+}
+
