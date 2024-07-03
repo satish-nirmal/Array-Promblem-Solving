796ba90e7b7f03e19811193c04c00a236c71cc3d
satish-nirmal <satishnirmal16@gmail.com>
2024-07-03 21:21:47 +0530

    Odd Even Number in Array

---
+++ OddEvenNumber.java
@@ -0,0 +1,22 @@
+package com.array.problems;
+
+public class OddEvenNumber {
+	public static void main(String[] args) {
+
+		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		for (int i = 0; i < a.length; i++) {
+
+			if (a[i] % 2 == 0) {
+				System.out.print(a[i] + " ");
+			}
+		}
+		System.out.println();
+		for (int i = 0; i < a.length; i++) {
+
+			if (a[i] % 2 != 0) {
+				System.out.print(a[i] + " ");
+			}
+		}
+	}
+}
