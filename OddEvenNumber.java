9ad2522222178603673942a23d5215efcc0d839d
satish-nirmal <satishnirmal16@gmail.com>
2024-09-01 20:19:26 +0530

    added commment on the progeam

--- OddEvenNumber.java
+++ OddEvenNumber.java
@@ -4,6 +4,8 @@ public class OddEvenNumber {
 	public static void main(String[] args) {
 
 		int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		
+		// this logic for odd and even numbers in above array[]
 
 		for (int i = 0; i < a.length; i++) {
 
